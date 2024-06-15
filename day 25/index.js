ddc3045678882db4e4b4d4420b9bdb8c49b8c5f3
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-16 02:16:57 +0500

    done

---
+++ day 25/index.js
@@ -0,0 +1,34 @@
+// QUESTION 73
+function updation() {
+    var iceCream = "jetspot";
+    console.log(iceCream);
+    iceCream = "chocolate";
+    console.log(iceCream);
+}
+updation();
+// *************************************************************************
+// QUESTION 74
+function swaping() {
+    var a = 5;
+    var b = 10;
+    console.log("before swaping a =" + a, "b =" + b);
+    var swap = a;
+    a = b;
+    b = swap;
+    console.log("after swaping a =" + a, "b =" + b);
+}
+swaping();
+// QUESTION 75
+function operators() {
+    var x = 90;
+    console.log(x);
+    x += 1;
+    console.log(x);
+    x -= 2;
+    console.log(x);
+    x *= 3;
+    console.log(x);
+    x /= 4;
+    console.log(x);
+}
+operators();
---
+++ day 25/index.ts
@@ -0,0 +1,50 @@
+// QUESTION 73
+
+function updation(){
+    let iceCream = "jetspot";
+    console.log(iceCream);
+
+    iceCream = "chocolate";
+    console.log(iceCream);
+    
+}
+updation()
+
+// *************************************************************************
+
+// QUESTION 74
+
+function swaping(){
+    let a = 5
+    let b = 10
+    console.log("before swaping a =" + a ,"b =" + b);
+
+    let swap = a
+    a = b
+    b = swap
+    console.log("after swaping a =" + a ,"b =" + b);
+}
+
+swaping()
+
+// *************************************************************************
+
+// QUESTION 75
+
+function operators(){
+    let x = 90
+    console.log(x);
+    x += 1
+    console.log(x);
+    x -= 2
+    console.log(x);
+    x *= 3
+    console.log(x);
+    x /= 4
+    console.log(x);
+}
+operators()
+
+// *************************************************************************
+
+
