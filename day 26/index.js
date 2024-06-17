aeca7ad23aad3939115670a759bc917699e99fc0
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-17 21:20:38 +0500

    done

---
+++ day 26/index.js
@@ -0,0 +1,17 @@
+// QUESTION 77
+function greeting(name) {
+    if (name === void 0) { name = "aftab"; }
+    console.log("hello, ".concat(name));
+}
+greeting("Zeeshan");
+greeting();
+// ****************************************************
+// QUESTION 78
+function getSqaure1(number) {
+    return number * number;
+}
+console.log(getSqaure1(9));
+function getSqaure2(number) {
+    return number * number;
+}
+console.log(getSqaure1(6));
---
+++ day 26/index.ts
@@ -0,0 +1,26 @@
+// QUESTION 77
+
+function greeting(name: string = "aftab"){
+    console.log(`hello, ${name}`);
+    
+}
+
+greeting("Zeeshan");
+greeting();
+
+// ****************************************************
+
+// QUESTION 78
+
+function getSqaure1(number: number,):number{
+    return number * number;
+}
+console.log(getSqaure1(9));
+
+
+function getSqaure2(number: number,):number{
+    return number * number;
+}
+console.log(getSqaure1(6));
+
+// ****************************************************
