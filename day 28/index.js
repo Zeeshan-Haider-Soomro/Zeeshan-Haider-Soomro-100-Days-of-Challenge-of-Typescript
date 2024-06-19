97ffa70a378d8ad3fca0fe67ac96442dac4bc95a
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-19 20:29:59 +0500

    done

---
+++ day 28/index.js
@@ -0,0 +1,19 @@
+// QUESTION 82
+function findString(str) {
+    return str.length;
+}
+console.log(findString("Hello World"));
+// ********************************************
+// QUESTION 83
+function caseChange(str) {
+    var uppercase = str.toUpperCase();
+    var lowercase = str.toLowerCase();
+    console.log(uppercase, lowercase);
+}
+caseChange("laibe Siddique");
+// ********************************************
+// QUESTION 84
+function replaceWord(sentence) {
+    return sentence.replace(/javascript/g, "typescript");
+}
+console.log(replaceWord("I love JavaScript and JavaScript is awesome!"));
---
+++ day 28/index.ts
@@ -0,0 +1,29 @@
+// QUESTION 82
+
+function findString(str: string){
+    return str.length
+}
+
+console.log(findString("Hello World"))
+
+// ********************************************
+
+// QUESTION 83
+
+function caseChange(str: string,){
+    let uppercase = str.toUpperCase()
+    let lowercase = str.toLowerCase()
+    console.log(uppercase, lowercase);
+}
+
+caseChange("laibe Siddique")
+
+// ********************************************
+
+// QUESTION 84
+
+function replaceWord(sentence: string): string {
+    return sentence.replace(/javascript/g, "typescript")
+}
+
+console.log(replaceWord("I love JavaScript and JavaScript is awesome!"))
