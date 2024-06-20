9d225523f3ee21d425d978f9c94c4b007c96fc48
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-21 01:24:11 +0500

    done

---
+++ day 30/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// QUESTION 88
+function roundedValue(number) {
+    return Math.round(number);
+}
+console.log(roundedValue(9.2));
+console.log(roundedValue(5.8));
+// ******************************************************
+// QUESTION 89
+function convertNumber(str) {
+    return parseFloat(str);
+}
+console.log(convertNumber("123.78"));
+// ******************************************************
+// QUESTION 90
+function isValueNaN(value) {
+    return isNaN(value);
+}
+console.log(isValueNaN("zeeshan"));
---
+++ day 30/index.ts
@@ -0,0 +1,27 @@
+// QUESTION 88
+
+function roundedValue(number: number){
+    return Math.round(number);
+}
+console.log(roundedValue(9.2));
+console.log(roundedValue(5.8));
+
+// ******************************************************
+
+// QUESTION 89
+
+function convertNumber(str: string){
+    return parseFloat(str)
+}
+console.log(convertNumber("123.78"));
+
+// ******************************************************
+
+// QUESTION 90
+
+function isValueNaN(value: any): boolean{
+    return isNaN(value);
+}
+console.log(isValueNaN("zeeshan"));
+
+
