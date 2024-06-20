e1b61770d243b0efbf9a622a73ab129879b56d42
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-20 20:02:39 +0500

    done

---
+++ day 29/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// QUESTION 85
+// function findIndex(str: string){
+//     return str.indexOf("aesa");
+// }
+// console.log(findIndex("insha'Allah aesa kuch nahi hone wala!!"));
+// *******************************************************************
+// QUESTION 86
+function checkWord(str) {
+    return str.includes("zain");
+}
+console.log(checkWord("is zain my top listed friend?"));
+// ********************************************************************
+// QUESTION 87
+function copyWord(str) {
+    return str.slice(0, 8);
+}
+console.log(copyWord("pakistan is the 2nd biggest muslim country in the world"));
---
+++ day 29/index.ts
@@ -0,0 +1,24 @@
+// QUESTION 85
+
+function findIndex(str: string){
+    return str.indexOf("aesa");
+}
+console.log(findIndex("insha'Allah aesa kuch nahi hone wala!!"));
+
+// *******************************************************************
+
+// QUESTION 86
+
+function checkWord(str: string):boolean{
+    return str.includes("zain")
+}
+console.log(checkWord("is zain my top listed friend?"));
+
+// ********************************************************************
+
+// QUESTION 87
+
+function copyWord(str: string){
+    return str.slice(0,8)
+}
+console.log(copyWord("pakistan is the 2nd biggest muslim country in the world"));
