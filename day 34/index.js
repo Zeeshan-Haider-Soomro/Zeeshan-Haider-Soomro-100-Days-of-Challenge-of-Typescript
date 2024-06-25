3486867d57befb175ec6eb70af6b94c6ccf2d682
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-25 19:11:35 +0500

    done

---
+++ day 34/index.js
@@ -0,0 +1,14 @@
+// QUESTION 01
+var sqaure = Math.sqrt(366);
+console.log(sqaure);
+// ****************************************************************************
+// QUESTION 02
+function generateNumber() {
+    var num = Math.floor(Math.random() * 100);
+    return num;
+}
+console.log(generateNumber());
+// ****************************************************************************
+// QUESTION 03
+var numb = Math.abs(-5 - 6);
+console.log(numb);
---
+++ day 34/index.ts
@@ -0,0 +1,35 @@
+// QUESTION 01
+
+let sqaure:number = Math.sqrt(366)
+console.log(sqaure);
+
+// ****************************************************************************
+
+// QUESTION 02
+
+function generateNumber(){
+    let num = Math.floor(Math.random() * 100);
+    return num;
+}
+
+console.log(generateNumber());
+
+// ****************************************************************************
+
+// QUESTION 03
+
+let numb = Math.abs(-5 - 6)
+console.log(numb);
+
+
+
+
+
+
+
+
+
+
+
+
+
