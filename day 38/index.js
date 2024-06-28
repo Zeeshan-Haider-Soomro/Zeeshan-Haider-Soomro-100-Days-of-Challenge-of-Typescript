944729e59871b11c432858ff82b9b79d2e57c561
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-28 16:18:17 +0500

    done

---
+++ day 38/index.js
@@ -0,0 +1,6 @@
+// QUESTION 112
+var countries = new Map();
+countries.set("Pakistan", "Islamabad");
+countries.set("India", "New Delhi");
+countries.set("bangladesh", "Dhaka");
+console.log(countries);
---
+++ day 38/index.ts
@@ -0,0 +1,12 @@
+// QUESTION 112
+
+let countries = new Map<string, string>();
+
+countries.set("Pakistan", "Islamabad");
+
+countries.set("India", "New Delhi");
+
+countries.set("bangladesh", "Dhaka");
+
+console.log(countries);
+
