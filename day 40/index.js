dd15e5ae12bdd753bf45b886c67d41778340736d
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-06-30 21:53:21 +0500

    done

---
+++ day 40/index.js
@@ -0,0 +1,17 @@
+// QUESTION 01
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+// ****************************************************************
+// QUESTION 02
+var strings = 1;
+while (strings < 6) {
+    console.log("hello world!");
+    strings++;
+}
+// ****************************************************************
+// QUESTION 03
+var arr = ["border in alice", "all us are dead", "madmax furie", "anime", "squid game"];
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
---
+++ day 40/index.ts
@@ -0,0 +1,35 @@
+// QUESTION 01
+
+for(let i = 0; i <10; i++) {
+    console.log(i);
+}
+
+// ****************************************************************
+
+// QUESTION 02
+
+let strings = 1
+
+while(strings<6){
+    console.log("hello world!");
+    strings++
+}
+
+// ****************************************************************
+
+// QUESTION 03
+
+let arr = ["border in alice", "all us are dead", "madmax furie","anime", "squid game"]
+
+for(let i = 0; i <arr.length; i++){
+    console.log(arr[i]);
+    
+}
+
+// ****************************************************************
+
+
+
+
+
+
